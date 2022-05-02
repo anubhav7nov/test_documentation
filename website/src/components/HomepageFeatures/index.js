695f35adb6f2b45c9c871412ad8cc036675c2c54
Anubhav Gupta <anubhav.gupta@go-jek.com>
2022-05-02 19:34:50 +0530

    added href

--- website/src/components/HomepageFeatures/index.js
+++ website/src/components/HomepageFeatures/index.js
@@ -40,6 +40,7 @@ function Feature({Svg, title, description}) {
     <div className={clsx('col col--4')}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
+        href: 'https://github.com/facebook/docusaurus'
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
